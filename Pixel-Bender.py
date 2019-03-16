b735159c3d6d89b2cfa05239ca86a5065dc97e9a
Bryan Wang <40893169+Bryan-Wang-105@users.noreply.github.com>
2019-03-16 15:03:59 -0400

    Initial Script
    V.1.0.0

---
+++ Pixel-Bender.py
@@ -0,0 +1,39 @@
+# OPENCV Practice Code
+
+# Imports necessary packages
+import numpy as np
+import cv2
+import time
+
+# caps is the mp4 object
+capture = cv2.VideoCapture(0)
+
+# create background subtractor
+fgbg = cv2.createBackgroundSubtractorMOG2()
+
+
+# sets first_frame equal to initial frame in mp4 object
+_, first_frame = capture.read()
+
+# Runs for 30 seconds
+t_end = time.time() + 60
+
+while time.time() < t_end:
+    # sets frame equal to this loop iteration's frame in the mp4
+    _, frame = capture.read()
+
+    fgmask = fgbg.apply(frame)
+
+    diff = cv2.absdiff(first_frame,frame)
+
+    # displays feeds
+    #cv2.imshow("Standard", frame)
+    cv2.imshow("Differences", diff)
+    cv2.imshow("Bkgrnd-Rmvl", fgmask)
+
+    first_frame = frame
+
+    key = cv2.waitKey(30)  #if key == 27:#    break
+
+capture.release()
+cv2.destroyAllWindows()
